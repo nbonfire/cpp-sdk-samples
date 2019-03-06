c9227b8b1eb75035b4eb2e0e2aeb1ce7d818dcc6
AffectivaNickB <nick.bonfatti@affectiva.com>
2019-03-06 11:50:40 -0500

    Update PlottingImageListener.h
    Add nan for mood, dominant emotion, dominant emotion confidence

--- vision/shared/PlottingImageListener.h
+++ vision/shared/PlottingImageListener.h
@@ -109,6 +109,8 @@ public:
             for (const auto& angle : viz.HEAD_ANGLES) out_stream << "nan,";
             for (const auto& emotion : viz.EMOTIONS) out_stream << "nan,";
             for (const auto& expression : viz.EXPRESSIONS) out_stream << "nan,";
+            out_stream << timeStamp
+                << "nan,nan,nan,"; // mood, dominant emotion, dominant emotion confidence
             out_stream << std::endl;
         }
 
